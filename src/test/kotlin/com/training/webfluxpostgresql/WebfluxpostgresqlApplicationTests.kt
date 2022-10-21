9d9855434429d4227d8da14724a32ed2831dd582
RamiroDL <83599443+RamiroDL@users.noreply.github.com>
2022-10-21 19:13:26 -0300

    ADD project setup

---
+++ src/test/kotlin/com/training/webfluxpostgresql/WebfluxpostgresqlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.training.webfluxpostgresql
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebfluxpostgresqlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
